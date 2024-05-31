ddf0fae9b0dd815f0f34cc87c997aca751209c3f
Motunrayo Lasisi <mongotest52@gmail.com>
2024-05-31 12:28:36 +0000

    test000

--- models/staging/jaffle_shop/stg_jaffle_shop_orders.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop_orders.sql
@@ -1,3 +1,5 @@
+
+
 select
     id as order_id,
     user_id as customer_id,
@@ -5,3 +7,9 @@ select
     status,
     _etl_loaded_at
 from {{ source('jaffle_shop', 'orders') }}
+
+{% if is_incremental() %}
+
+  where _etl_loaded_at >= (select max(_etl_loaded_at) from {{ this }} )
+
+{% endif %}
